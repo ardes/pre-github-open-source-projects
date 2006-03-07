92c05b28b30f6c94d73cf77d05e799357c9ee6ec
ian <ian@6e3d003c-23fb-0310-a867-ecd3a34eed4b>
2006-03-07 11:38:53 +0000

    Moved initialize and continuation_dispatch catch to perform_action (hook).  Now continuation_call need not be explicitly within a continuation block.
    git-svn-id: http://svn.ardes.com/ardes@39 6e3d003c-23fb-0310-a867-ecd3a34eed4b

--- rails/trunk/lib/ardes/action_controller/continuation.rb
+++ rails/trunk/lib/ardes/action_controller/continuation.rb
@@ -9,60 +9,57 @@ module Ardes
 
       module ClassMethods
         def has_continuations
-          before_filter :continuation_initialize
           include InstanceMethods
         end
       end
       
       module InstanceMethods
-      private
-        def continuation_initialize
-          session[:continuation_handler] = Handler.new unless session[:continuation_handler]
-          @continuations = session[:continuation_handler]
-          @continuations.set_current(self)
-          continuation_dispatcher { @continuations.pop(self) }
+        def self.included(base)
+          base.send :alias_method, :perform_action_without_continuations, :perform_action
+          base.send :alias_method, :perform_action, :perform_action_with_continuations
         end
 
-        def continuation_dispatcher
-          result = catch :continuation_dispatch do
-            yield
+      public
+        def perform_action_with_continuations
+          continuation_initialize
+          page = catch :continuation_dispatch do
+            return perform_action_without_continuations
           end
-          if result.is_a? Page
-            redirect_to result.redirect_options
-          end
+          redirect_to page.redirect_options
         end
-        
-        def continuation
-          continuation_dispatcher do
-            yield
-            @continuations.cleanup
-          end
+
+      private
+        def continuation_initialize
+          session['__continuation_handler'] = Handler.new unless session['__continuation_handler']
+          @continuation_handler = session['__continuation_handler']
+          @continuation_handler.set_current(self)
+          @continuation_handler.pop(self)
         end
-        
+
         def continuation_call(options)
-          @continuations.call(Page.new(options, self), self)
+          @continuation_handler.call(Page.new(options, self), self)
         end
-        
+
         def continuation_return(result, target = nil)
           if target.is_a? Hash
             target = Page.new(target, self)
           elsif target.nil?
             target = Page.current(self)
           end
-          continuation_dispatcher do
-            @continuations.return_result(result, target)
-          end
+          @continuation_handler.return_result(result, target)
         end
-        
+
         def continuation_return_page
           target = Page.current(self)
-          continuation_dispatcher do
-            @continuations.check_target_is_top(target)
-          end
+          @continuation_handler.check_target_is_top(target)
           target
         end
+        
+        def continuation_cleanup(to_clean = nil)
+          @continuation_handler.cleanup(to_clean)
+        end
       end
-      
+
       class Handler
         attr_accessor :states, :results, :current
           
@@ -74,7 +71,7 @@ module Ardes
         def set_current(controller)
           @current = Page.current(controller)
         end
-        
+
         def call(target, controller)
           catch :no_result do
             return result_for(target)
@@ -82,14 +79,14 @@ module Ardes
           @states.push(State.new(@current, target, controller))
           throw :continuation_dispatch, target
         end
-        
+
         def return_result(result, target)
           if check_target_is_top(target)
             @states.last.result = result
             throw :continuation_dispatch, @states.last.source
           end
         end
-        
+
         def pop(controller)
           if @states.last and @states.last.source.id == @current.id
             # check if we have a result, if not, dispatch to the target
@@ -103,7 +100,7 @@ module Ardes
             @results[state.source.id][state.target.id] = state.result
           end
         end
-        
+
         def check_target_is_top(target)
           if target.id == @states.last.target.id
             true
@@ -111,7 +108,7 @@ module Ardes
             throw :continuation_dispatch, @states.last.target
           end
         end
-        
+
         def result_for(target)
           if @results.key?(@current.id) and @results[@current.id].key?(target.id)
             return @results[@current.id][target.id]
@@ -119,7 +116,7 @@ module Ardes
             throw :no_result
           end
         end
-        
+
         def cleanup(to_clean = nil)
           if to_clean == :all
             @results = Hash.new
@@ -131,7 +128,7 @@ module Ardes
           end
         end
       end
-      
+
       class State
         attr_reader :result, :source, :target, :params
         
