c8f3dc84881d4d0fec3ed5732b92095894300fee
ian <ian@6e3d003c-23fb-0310-a867-ecd3a34eed4b>
2006-05-09 21:38:39 +0000

    Adding validatable and acts_as_tableless
    git-svn-id: http://svn.ardes.com/ardes@59 6e3d003c-23fb-0310-a867-ecd3a34eed4b

---
+++ rails/ardes/vendor/plugins/acts_as_tableless/lib/acts_as_tableless.rb
@@ -0,0 +1,62 @@
+module ActiveRecord# :nodoc:
+  module Acts# :nodoc
+    module Tableless
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+      
+      module ClassMethods
+        #
+        # Specify that this active record works without a table in the db
+        # Specifying columns uses the same syntax as for migrations
+        # 
+        # usage:
+        #   acts_as_tableless :my_col, :my_other_col
+        #
+        #   acts_as_tableless do
+        #     column :my_col, :integer, :null => false
+        #     column :my_other_col, :string, :default => 'hello!', :null => true
+        #   end
+        def acts_as_tableless(*columns)
+          include ActMethods
+          columns.each {|c| column(c)}
+          yield if block_given?
+        end
+      end
+      
+      module ActMethods
+        def self.included(base)
+          base.class_eval do
+            extend ClassMethods
+            alias_method :save, :valid?
+          end
+        end
+
+        module ClassMethods
+          def columns
+            @columns ||= []
+          end
+          
+          def column(name, type = nil, options = {})
+            columns << Column.new(name.to_s, type, options)
+          end
+        end
+      end
+      
+      class Column < ActiveRecord::ConnectionAdapters::Column
+        def initialize(name, type = nil, options = {})
+          @name    = name
+          @type    = type
+          @null    = options[:null]
+          @default = type_cast(options[:default])
+          @limit   = options[:limit]
+          @primary = nil
+          @text    = [:string, :text].include? @type
+          @number  = [:float, :integer].include? @type
+        end
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.class_eval { include ActiveRecord::Acts::Tableless }
---
+++ rails/ardes/vendor/plugins/validatable/test/test_validatable.rb
@@ -0,0 +1,68 @@
+# unit test extension for validatable mixin
+#
+# assumes that there are attribute accessors for the target class
+#
+module TestValidatable
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
+  module ClassMethods
+    def test_validatable(target_class, valid_attrs, invalid_attrs)
+      include InstanceMethods
+      cattr_accessor :test_validatable_class, :test_validatable_valid_attrs, :test_validatable_invalid_attrs
+      self.test_validatable_class         = target_class
+      self.test_validatable_valid_attrs   = valid_attrs
+      self.test_validatable_invalid_attrs = invalid_attrs
+    end
+  end
+  
+  module InstanceMethods
+    def test_validatable_valid_eh
+      obj = self.test_validatable_class.new
+      assert obj.respond_to?('valid?')
+    end
+    
+    def test_validatable_method_missing_should_respond_to_before_type_cast
+      obj = self.test_validatable_class.new
+      self.test_validatable_valid_attrs.each do |a,v|
+        obj.send(a.to_s + "=",v)
+        assert_equal obj.send(a), obj.send(a.to_s + "_before_type_cast")
+      end
+    end
+    
+    def test_validatable_class_human_attribute_name
+      assert_equal "My attr", self.test_validatable_class.human_attribute_name("my_attr")
+    end
+    
+    def test_validatable_respond_to_eh_methods_required_for_validation
+      obj = self.test_validatable_class.new
+      [:save, :update_attribute, :save!].each do |method|
+        assert obj.respond_to?(method)
+      end
+    end
+    
+    def test_validatable_index
+      obj = self.test_validatable_class.new
+      
+      self.test_validatable_valid_attrs.each do |a,v|
+        obj.send(a.to_s + "=", v)
+        assert_equal v, obj[a]
+      end
+    end
+    
+    def test_validatable_should_validate_object_with_valid_attributes
+      obj = self.test_validatable_class.new
+      self.test_validatable_valid_attrs.each {|a,v| obj.send(a.to_s + "=",v)}
+      assert obj.valid?
+    end
+
+    def test_validatable_should_not_validate_object_with_invalid_attributes
+      obj = self.test_validatable_class.new
+      self.test_validatable_invalid_attrs.each {|a,v| obj.send(a.to_s + "=",v)}
+      assert(!obj.valid?)
+    end
+  end
+end
+
+Test::Unit::TestCase.class_eval { include TestValidatable }
