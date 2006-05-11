ad5cd2cf79a7b7bf622437be46f9a27ce7192dc1
ian <ian@6e3d003c-23fb-0310-a867-ecd3a34eed4b>
2006-05-11 08:24:30 +0000

    Small refactorings
    git-svn-id: http://svn.ardes.com/ardes@72 6e3d003c-23fb-0310-a867-ecd3a34eed4b

--- rails/plugins/acts_as_tableless/test/test_acts_as_tableless.rb
+++ rails/plugins/acts_as_tableless/test/test_acts_as_tableless.rb
@@ -10,30 +10,30 @@ module TestActsAsTableless
   module ClassMethods
     def test_acts_as_tableless(target_class)
       include InstanceMethods
-      cattr_accessor :test_acts_as_tableless_class
-      self.test_acts_as_tableless_class = target_class
+      cattr_accessor :acts_as_tableless_class
+      self.acts_as_tableless_class = target_class
     end
   end
   
   module InstanceMethods
     def test_acts_as_tableless_should_have_attributes_corresponding_to_columns
-      obj = self.test_acts_as_tableless_class.new
+      obj = self.acts_as_tableless_class.new
       assert_equal obj.attributes.keys.sort, obj.class.columns.collect{|c| c.name}.sort
     end
 
     def test_acts_as_tableless_should_make_new_object_on_create
-      assert_kind_of self.test_acts_as_tableless_class, self.test_acts_as_tableless_class.create
+      assert_kind_of self.acts_as_tableless_class, self.acts_as_tableless_class.create
     end
 
     def test_acts_as_tableless_should_make_object_corresponding_to_attributes_on_update_attributes
-      obj = self.test_acts_as_tableless_class.new
+      obj = self.acts_as_tableless_class.new
       attr = obj.attributes.keys.first
       obj.update_attributes(attr => '1')
       assert_equal '1', obj.attributes[attr].to_s
     end
 
     def test_acts_as_tableless_should_not_raise_error_on_save
-      self.test_acts_as_tableless_class.new.save
+      self.acts_as_tableless_class.new.save
     end
   end
 end
