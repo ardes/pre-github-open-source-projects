080487c35227fdc10aaca7b098ded4ee9711ce5c
ian <ian@6e3d003c-23fb-0310-a867-ecd3a34eed4b>
2006-05-11 10:08:01 +0000

    Added crud abstract test
    git-svn-id: http://svn.ardes.com/ardes@75 6e3d003c-23fb-0310-a867-ecd3a34eed4b

---
+++ rails/plugins/test_crud/lib/test_crud.rb
@@ -0,0 +1,70 @@
+#
+# To test basic crud operations on your active record do the following
+#
+# class MyTest < Test::Unit::TestCase
+#   test_crud ClassName, :a_fixture_name, {:an_attr => 'a val for create', ...}
+# end
+#
+module Test
+  module Abstract
+    module Crud
+      def self.included(base)
+        super
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        def test_crud(crud_class, crud_fixture, crud_attrs = nil)
+          include InstanceMethods
+          self.class_eval do
+            cattr_accessor :crud_class, :crud_fixture, :crud_attrs
+            self.crud_fixture = crud_fixture.to_s
+            self.crud_class   = crud_class
+            self.crud_attrs   = crud_attrs
+          end
+        end
+      end
+
+      module InstanceMethods
+        # this test only checks read on non aggregate and non associate attributes
+        # write your own tests for these
+        def test_crud_should_perform_read
+          fixture = send(self.crud_class.table_name, self.crud_fixture)
+          object  = self.crud_class.find(fixture.id)
+
+          assert_kind_of self.crud_class, object
+          reflections = self.crud_class.reflections.keys
+          self.crud_attrs.keys.each do |attr|
+            assert_equal fixture[attr], object.send(attr) unless reflections.include? attr
+          end
+        end
+
+        def test_crud_should_perform_create
+          object = self.crud_class.create(self.crud_attrs).reload
+
+          assert_kind_of self.crud_class, object
+          self.crud_attrs.each do |attr, value|
+            assert_equal value, object.send(attr)
+          end
+        end
+
+        def test_crud_should_perform_update
+          object = self.crud_class.find_first
+
+          object.update_attributes(self.crud_attrs)
+          object.reload
+          self.crud_attrs.each do |attr, value|
+            assert_equal value, object.send(attr)
+          end
+        end
+
+        def test_crud_should_perform_destroy
+          obj = self.crud_class.find_first.destroy
+          assert_raise(::ActiveRecord::RecordNotFound) { self.crud_class.find(obj.id) }
+        end
+      end
+    end
+  end
+end
+
+Test::Unit::TestCase.class_eval { include Test::Abstract::Crud }
