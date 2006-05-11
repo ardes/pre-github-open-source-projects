cd01027b6bd811304170fde8faad7a4e73fd3214
ian <ian@6e3d003c-23fb-0310-a867-ecd3a34eed4b>
2006-05-11 19:26:38 +0000

    Added validates_part plugin
    git-svn-id: http://svn.ardes.com/ardes@82 6e3d003c-23fb-0310-a867-ecd3a34eed4b

---
+++ rails/plugins/validates_part/lib/validates_part.rb
@@ -0,0 +1,14 @@
+class ActiveRecord::Base
+  # validates a model part (an aggregation, or association) storing the error messages under the name of the part
+  def self.validates_part(*attr_names)
+    configuration = {}
+    configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
+
+    validates_each(attr_names, configuration) do |record, attr_name, value|
+      dup_value = value.dup
+      if !dup_value.valid?
+        dup_value.errors.each { |attr, msg| record.errors.add(attr_name, attr.to_s.humanize + " " + msg) }
+      end
+    end
+  end
+end
