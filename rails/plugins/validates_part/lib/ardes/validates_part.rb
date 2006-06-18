f869c7e050560ad6514a03d5d6c03bddba96a774
ian <ian@6e3d003c-23fb-0310-a867-ecd3a34eed4b>
2006-06-18 15:10:31 +0000

    Added :prepend_attribute option to validates part
    git-svn-id: http://svn.ardes.com/ardes@134 6e3d003c-23fb-0310-a867-ecd3a34eed4b

--- rails/plugins/validates_part/lib/ardes/validates_part.rb
+++ rails/plugins/validates_part/lib/ardes/validates_part.rb
@@ -7,15 +7,21 @@ module Ardes
       
       module ClassMethods
         # validates a model part (an aggregation, or association) storing the error messages under the name of the part
+        # takes :prepend_attribute => true/false as an option (defaults to true)
         def validates_part(*attr_names)
           configuration = {}
           configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
+          
+          prepend_attribute = true if (prepend_attribute = configuration.delete(:prepend_attribute)).nil?
 
           validates_each(attr_names, configuration) do |record, attr_name, value|
             if value
               dup_value = value.dup
               if !dup_value.valid?
-                dup_value.errors.each { |attr, msg| record.errors.add(attr_name, attr.to_s.humanize + " " + msg) }
+                dup_value.errors.each do |attr, msg|
+                  prepend = prepend_attribute ? attr.to_s.humanize.downcase + ' ' : ''
+                  record.errors.add(attr_name, "#{prepend}#{msg}")
+                end
               end
             end
           end
