f95e0e13cd1999ee949952c2bc75c09c8c993b30
ian <ian@6e3d003c-23fb-0310-a867-ecd3a34eed4b>
2006-07-14 09:17:59 +0000

    improved trac_perms script
    git-svn-id: http://svn.ardes.com/ardes@155 6e3d003c-23fb-0310-a867-ecd3a34eed4b

--- sh/svn_trac_setup/svn_trac_setup.src/trac_perms.sh
+++ sh/svn_trac_setup/svn_trac_setup.src/trac_perms.sh
@@ -1,10 +1,20 @@
-trac-admin /srv/trac/#{$PROJECT} permission remove anonymous BROWSER_VIEW CHANGESET_VIEW FILE_VIEW LOG_VIEW MILESTONE_VIEW REPORT_SQL_VIEW REPORT_VIEW ROADMAP_VIEW SEARCH_VIEW TICKET_CREATE TICKET_MODIFY TICKET_VIEW TIMELINE_VIEW WIKI_CREATE WIKI_MODIFY WIKI_VIEW
+trac-admin /srv/trac/#{$PROJECT} permission remove anonymous *
+trac-admin /srv/trac/#{$PROJECT} permission remove developer *
+trac-admin /srv/trac/#{$PROJECT} permission remove designer *
+trac-admin /srv/trac/#{$PROJECT} permission remove client *
+trac-admin /srv/trac/#{$PROJECT} permission remove ian *
+trac-admin /srv/trac/#{$PROJECT} permission remove ray *
 
 trac-admin /srv/trac/#{$PROJECT} permission add developer TRAC_ADMIN
 
-trac-admin /srv/trac/#{$PROJECT} permission add designer TIMELINE_VIEW SEARCH_VIEW BROWSER_VIEW LOG_VIEW FILE_VIEW CHANGESET_VIEW WIKI_ADMIN MILESTONE_ADMIN ROADMAP_ADMIN TICKET_ADMIN
+trac-admin /srv/trac/#{$PROJECT} permission add designer
+  TIMELINE_VIEW SEARCH_VIEW BROWSER_VIEW LOG_VIEW FILE_VIEW CHANGESET_VIEW \
+  WIKI_ADMIN MILESTONE_ADMIN ROADMAP_ADMIN TICKET_ADMIN
 
-trac-admin /srv/trac/#{$PROJECT} permission add client BROWSER_VIEW CHANGESET_VIEW FILE_VIEW LOG_VIEW MILESTONE_VIEW REPORT_SQL_VIEW REPORT_VIEW ROADMAP_VIEW SEARCH_VIEW TICKET_CREATE TICKET_MODIFY TICKET_VIEW TIMELINE_VIEW WIKI_CREATE WIKI_MODIFY WIKI_VIEW
-
+trac-admin /srv/trac/#{$PROJECT} permission add client \
+  BROWSER_VIEW CHANGESET_VIEW FILE_VIEW LOG_VIEW MILESTONE_VIEW \
+  REPORT_SQL_VIEW REPORT_VIEW ROADMAP_VIEW SEARCH_VIEW TICKET_CREATE \
+  TICKET_MODIFY TICKET_VIEW TIMELINE_VIEW WIKI_CREATE WIKI_MODIFY WIKI_VIEW
+  
 trac-admin /srv/trac/#{$PROJECT} permission add ian developer
 trac-admin /srv/trac/#{$PROJECT} permission add ray designer
